1b38a8caa0d547dc5f010e11eaa11b487a2c9298
admin <ivanivanovkzv@gmail.com>
2014-11-13 13:39:01 +0500

    update

--- inject.js
+++ inject.js
@@ -1,6 +1,6 @@
 var _3s3sObject = 
 {
-	adNetworks = [
+	adNetworks: [
 		{host: "ad.a-ads.com", code: "<iframe data-aa='47233' src='//ad.a-ads.com/47233?size=468x60' scrolling='no' style='width:468px; height:60px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>"}
 	],
 	proxyList: ["3s3s.org", "unblok.us"],
