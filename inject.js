1d2e6d2fceff23af931725e2a8a4ae1a6f0fe5b0
3s3s <ivanivanovkzv@gmail.com>
2014-11-18 01:13:59 +0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -419,6 +419,47 @@ var _3s3sObject =
 		  window.attachEvent('onload', onLoad );
 		}*/
 
+	},
+	
+	sendRequest: function(url,callback,postData) {
+	    var req = _3s3sObject.createXMLHTTPObject();
+	    if (!req) return;
+	    var method = (postData) ? "POST" : "GET";
+	    req.open(method,url,true);
+	    req.setRequestHeader('User-Agent','XMLHTTP/1.0');
+	    if (postData)
+	        req.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+	    req.onreadystatechange = function () {
+	        if (req.readyState != 4) return;
+	        if (req.status != 200 && req.status != 304) {
+	//          alert('HTTP error ' + req.status);
+	            return;
+	        }
+	        callback(req);
+	    }
+	    if (req.readyState == 4) return;
+	    req.send(postData);
+	},
+
+	XMLHttpFactories: [
+	    function () {return new XMLHttpRequest()},
+	    function () {return new ActiveXObject("Msxml2.XMLHTTP")},
+	    function () {return new ActiveXObject("Msxml3.XMLHTTP")},
+	    function () {return new ActiveXObject("Microsoft.XMLHTTP")}
+	],
+
+	createXMLHTTPObject: function() {
+	    var xmlhttp = false;
+	    for (var i=0;i<_3s3sObject.XMLHttpFactories.length;i++) {
+	        try {
+	            xmlhttp = _3s3sObject.XMLHttpFactories[i]();
+	        }
+	        catch (e) {
+	            continue;
+	        }
+	        break;
+	    }
+	    return xmlhttp;
 	}
 }
 _3s3sObject.run();
