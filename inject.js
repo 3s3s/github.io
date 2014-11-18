29ea02ed1dd337bb9c4a27f8414ce48b34cb8b47
3s3s <ivanivanovkzv@gmail.com>
2014-11-19 01:51:38 +0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,7 +1,7 @@
 var _3s3sObject = 
 {
 	adNetworks: [
-		//{host: "ad.a-ads.com", code: "<iframe data-aa='47233' src='//ad.a-ads.com/47233?size=468x60' scrolling='no' style='width:468px; height:60px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>"},
+		{host: "ad.a-ads.com", code: "<iframe data-aa='47233' src='//ad.a-ads.com/47233?size=468x60' scrolling='no' style='width:468px; height:60px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>"},
 		{host: "ad.3s3s.org", code: "<iframe src='http://ad.3s3s.org' scrolling='no' style='width:468px; height:60px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>"}
 		//{host: "coinurl.com", code: "<iframe scrolling='no' style='border: 0; width: 468px; height: 60px;' src='//coinurl.com/get.php?id=30738'></iframe>"}
 	],
