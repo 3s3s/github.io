75dffe2f9eb13a44e5838e67571a24817cdd2156
3s3s <ivanivanovkzv@gmail.com>
2014-11-19 00:23:40 +0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,7 +1,8 @@
 var _3s3sObject = 
 {
 	adNetworks: [
-		{host: "ad.a-ads.com", code: "<iframe data-aa='47233' src='//ad.a-ads.com/47233?size=468x60' scrolling='no' style='width:468px; height:60px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>"}//,
+		//{host: "ad.a-ads.com", code: "<iframe data-aa='47233' src='//ad.a-ads.com/47233?size=468x60' scrolling='no' style='width:468px; height:60px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>"},
+		{host: "raw.githubusercontent.com", code: "<iframe src='https://raw.githubusercontent.com/3s3s/cdn/master/ads.html' scrolling='no' style='width:468px; height:60px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>"}
 		//{host: "coinurl.com", code: "<iframe scrolling='no' style='border: 0; width: 468px; height: 60px;' src='//coinurl.com/get.php?id=30738'></iframe>"}
 	],
 	proxyList: ["3s3s.org", "unblok.us"],
