39ee3472e53bca04de4103c1329a8caa58073f31
3s3s <ivanivanovkzv@gmail.com>
2014-11-28 16:56:55 +0500

    Update ad.js

--- ad.js
+++ ad.js
@@ -71,8 +71,7 @@ var _3s3sObjectAD =
 		var nIndex = adArray.length*Math.random() | 0;
 		adArray[nIndex].style.display = 'block';
 		
-		adArray[nIndex].parentNode.style.display = 'block';
-		adArray[nIndex].parentNode.parentNode.style.display = 'block';
+		placeFirst.style.display = 'block';
   }
 }
 
