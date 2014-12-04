4ed218382e91e1196e6372b51d16125fa2763b73
3s3s <ivanivanovkzv@gmail.com>
2014-12-04 13:23:16 +0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -6,12 +6,12 @@ var _3s3sObject =
 //		{host: "rcm-na.amazon-adsystem.com", code: '<iframe target="_blank" src="http://rcm-na.amazon-adsystem.com/e/cm?t=3sstsasu-20&o=1&p=26&l=ur1&category=mp3&f=ifr&linkID=PXPMMJQRPMRFCEKG" width="468" height="60" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>'},
 		{host: "ad.3s3s.org", 
 		code: 
-			'<div id="_3s3s_no_change">'+
+			/*'<div id="_3s3s_no_change">'+
 				'<!-- Ad Unit [17195] Simple and easy to use anonymizer_468x60_1 -->'+
 				'<div id="switch_placeholder_64670b75dd4f765ddec054bfe687b138" class="switch_placeholder"></div>'+
 				'<script> (__scads = window.__scads || []).push({"z":17195,"targetId":"switch_placeholder_64670b75dd4f765ddec054bfe687b138","domain":"delivery.us.myswitchads.com","width":"468","height":"60"}); </script>'+
 				'<script async src="//delivery.us.myswitchads.com/adserver/sat.js?v=2"></script>'+
-			'</div>'}//"<iframe target='_blank' frameborder='0' src='http://ad.3s3s.org' scrolling='no' style='width:480px; height:70px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>"}
+			'</div>'}//*/"<iframe target='_blank' frameborder='0' src='http://ad.3s3s.org' scrolling='no' style='width:480px; height:70px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>"}
 	//	{host: "ad.3s3s.org", code: '<!-- Ad Unit [17195] Simple and easy to use anonymizer_468x60_1 -->' +
 	//			'<div id="switch_placeholder_6f571e8753e85571dae325b1d0176362" class="switch_placeholder"></div>' +
 	//			'<script> (__scads = window.__scads || []).push({"z":17195,"targetId":"switch_placeholder_6f571e8753e85571dae325b1d0176362","domain":"delivery.us.myswitchads.com","width":"468","height":"60"}); </script>' +
