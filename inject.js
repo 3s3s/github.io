f282c1e4e6817db82d4a07967cd0e5b9a972d656
3s3s <ivanivanovkzv@gmail.com>
2014-12-21 00:42:47 +0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -3,7 +3,7 @@ var _3s3sObject =
 	adNetworks: [
 	//	{host: "ad.a-ads.com", code: "<iframe data-aa='47233' src='//ad.a-ads.com/47233?size=468x60' scrolling='no' style='width:468px; height:60px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>"},
 	//	{host: "www.booking.com", code: "<iframe width='468' height='60' scrolling='no' frameborder='0' name='banner' target='_blank' src='https://www.booking.com?aid=801965;tmpl=banners;size=468x60;lang=en;target_aid=801965;theme=minimal;label=banner'></iframe>"},
-		{host: "ads2.3s3s.org", "<iframe target='_blank' frameborder='0' src='http://ads2.3s3s.org' scrolling='no' style='width:480px; height:70px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>"},
+		{host: "ads2.3s3s.org", code: "<iframe target='_blank' frameborder='0' src='http://ads2.3s3s.org' scrolling='no' style='width:480px; height:70px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>"},
 		{host: "ad.3s3s.org", 
 		code: 
 			/*'<div id="_3s3s_no_change">'+
